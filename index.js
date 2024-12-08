8f9551e14973f261caad2acea9c60462067c3d9d
shoyonp <shoyonkumar143@gmail.com>
2024-12-08 23:31:51 +0600

    all done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
